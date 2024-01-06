ff0fe49c83be4aa7096ee2b1a8e1515e8b30f626
nithish01k <155215189+nithish01k@users.noreply.github.com>
2024-01-06 14:13:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,9 +44,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), True)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cliknot:8270008063@cluster0990.ujysa9z.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0990")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ipoopudig')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Fireboy:harii@fucking.guwxy7e.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "fucking")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ipopudig')
 
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
